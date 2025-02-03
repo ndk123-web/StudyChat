bf00ad444d3e6b8c41c30af0bb41f4fe520e0423
ndk <navnathkadam284@gmail.com>
2025-02-03 19:17:51 +0530

    install_whitenoise

--- Django/settings.py
+++ Django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
