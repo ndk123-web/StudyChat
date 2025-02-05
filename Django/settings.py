46a4648aaa39b2e9ad4eae3fc76e74f6356751fb
Navnath Kadam <navnathkadam284@gmail.com>
2025-02-05 13:16:31 +0530

    Update settings.py

--- Django/settings.py
+++ Django/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import 
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
